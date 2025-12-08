c19f49ade2efba10a1c1afa852e3bb4f4dd06973
riina <54872398+riina@users.noreply.github.com>
2025-12-07 19:30:31 -0800

    update test

--- src/EA.Tests/Tests.cs
+++ src/EA.Tests/Tests.cs
@@ -7,7 +7,7 @@ namespace EA.Tests;
 public class Tests
 {
     [Test]
-    public void TestCategoriesFromData()
+    public void ParsedData_AllValuesResolveCorrectly()
     {
         var categories = Catter.Categorize(File.OpenRead("EastAsianWidth.txt"), out _, out _, out _, out _);
         foreach (CRange range in categories)
@@ -15,6 +15,27 @@ public class Tests
             EastAsianWidthKind kind = range.Kind;
             foreach (int i in range) Assert.That(EastAsianWidth.GetWidthKind(i), Is.EqualTo(kind), () => $"Failed check on character index {i} in range {range}");
         }
+
         Assert.Pass();
     }
+
+    [Test]
+    public void TestValue_HasCorrectResolution([ValueSource(nameof(s_testValues))] KnownEastAsianWidthInfo testValue)
+    {
+        Assert.That(EastAsianWidth.GetWidthKind(testValue.CodePoint, 0), Is.EqualTo(testValue.Kind));
+    }
+
+    private static readonly KnownEastAsianWidthInfo[] s_testValues =
+    [
+        new("a", EastAsianWidthKind.Narrow), //
+        new("力", EastAsianWidthKind.Wide), //
+        new("\u23F0", EastAsianWidthKind.Wide), //
+        new("\uFF0B", EastAsianWidthKind.Fullwidth), //
+        new("\u20A9", EastAsianWidthKind.Halfwidth), //
+        new("\u00A9", EastAsianWidthKind.Neutral), //
+        new("\u2200", EastAsianWidthKind.Ambiguous), //
+        new("\U000F0000", EastAsianWidthKind.Ambiguous), // "All private-use characters are by default classified as Ambiguous, because their definition depends on context."
+    ];
 }
+
+public record KnownEastAsianWidthInfo(string CodePoint, EastAsianWidthKind Kind);
